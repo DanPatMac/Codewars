d913013bbb58158bb5540ae75bfde3a3f607e547
DanPatMac <36416478+DanPatMac@users.noreply.github.com>
2018-02-12 14:35:00 -0600

    Update populationGrowth.js

--- JavaScript/populationGrowth.js
+++ JavaScript/populationGrowth.js
@@ -33,6 +33,7 @@ function nbYear(p0, percent, aug, p) {
   return i; //Returns year(increment)
 }
 
-    console.log(nbYear(1500, 5, 100, 5000)==15);
-    console.log(nbYear(1500000, 2.5, 10000, 2000000)==10);
-    console.log(nbYear(1500000, 0.25, 1000, 2000000)==94);
+//Test Cases
+console.log(nbYear(1500, 5, 100, 5000)==15);
+console.log(nbYear(1500000, 2.5, 10000, 2000000)==10);
+console.log(nbYear(1500000, 0.25, 1000, 2000000)==94);
